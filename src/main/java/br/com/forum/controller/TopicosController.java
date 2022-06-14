ad97306d2db3e70b6adda4b7ab987eac3f6dea97
Bernardo Duarte <bernardo.silva@infobase.com.br>
2022-06-14 18:23:51 -0300

    criacao de repository

--- src/main/java/br/com/forum/controller/TopicosController.java
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -1,23 +1,25 @@
 package br.com.forum.controller;
 
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.forum.controller.dto.TopicoDto;
-import br.com.forum.modelo.Curso;
-import br.com.forum.modelo.Topico;
+import br.com.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("topicos")
 public class TopicosController {
 	
-	@RequestMapping("/topicos")
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
+	@GetMapping
 	public List<TopicoDto> lista() {
-		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-		
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		return TopicoDto.converter(topicoRepository.findAll());
 	}
 }
