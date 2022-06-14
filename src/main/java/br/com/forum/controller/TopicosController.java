6a5d98931aca4947c7ec17f9893ee2743e2a3d4f
Bernardo Duarte <bernardo.silva@infobase.com.br>
2022-06-14 18:37:54 -0300

    novos recusos

--- src/main/java/br/com/forum/controller/TopicosController.java
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class TopicosController {
 	@GetMapping
 	public List<TopicoDto> lista() {
 		return TopicoDto.converter(topicoRepository.findAll());
+	}
+	
+	@GetMapping("{nomeCurso}")
+	public List<TopicoDto> bucarPorNome(@PathVariable final String nomeCurso) {
+		return TopicoDto.converter(topicoRepository.findByCursoNome(nomeCurso));
 	}
 }
