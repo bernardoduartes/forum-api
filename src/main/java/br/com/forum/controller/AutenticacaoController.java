927083a5daaa467fdce2c1d1daa43661de82e467
Bernardo Duarte <bernardo.silva@infobase.com.br>
2022-06-15 13:10:12 -0300

    Gerando tokens com JWT

--- src/main/java/br/com/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/forum/controller/AutenticacaoController.java
@@ -2,12 +2,18 @@ package br.com.forum.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.forum.config.security.TokenService;
 import br.com.forum.controller.dto.TokenDto;
 import br.com.forum.controller.form.LoginForm;
 
@@ -15,14 +21,23 @@ import br.com.forum.controller.form.LoginForm;
 @RequestMapping("auth")
 public class AutenticacaoController {
 	
+	@Autowired
+	private AuthenticationManager authManager;
+
+	@Autowired
+	private TokenService tokenService;
 	
 	@PostMapping
 	public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form) {
+		UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 		
-		System.out.println(form.getEmail());
-        System.out.println(form.getSenha());
-
-        return ResponseEntity.ok().build();
+		try {
+			Authentication authentication = authManager.authenticate(dadosLogin);
+			String token = tokenService.gerarToken(authentication);
+			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+		} catch (AuthenticationException e) {
+			return ResponseEntity.badRequest().build();
+		}
 	}
 
 }
