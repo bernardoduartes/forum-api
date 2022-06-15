91e19aae0439ec770fd1b5d364057795d32c5173
Bernardo Duarte <bernardo.silva@infobase.com.br>
2022-06-14 23:14:21 -0300

    Autenticacao

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$sWBvvEKgu3IocES3tU4q2OtuWTdLW8ybOtT1Ow7prWbvn5MPThDde');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
