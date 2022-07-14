7c8c27d919438041bfa2296b7b32ab985986220e
Bernardo Duarte <bernardo.silva@infobase.com.br>
2022-07-14 11:30:36 -0300

    Testando um repository

--- src/test/java/br/com/forum/ForumApiApplicationTests.java
+++ src/test/java/br/com/forum/ForumApiApplicationTests.java
@@ -1,13 +1,18 @@
 package br.com.forum;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ForumApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.assertTrue(true);
 	}
 
 }
