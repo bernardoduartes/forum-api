3a130f6c9e1a5165fa534be33fedfde73e602fff
Bernardo Duarte <bernardo.silva@infobase.com.br>
2022-07-14 13:44:13 -0300

    ajustes no teste

--- src/test/java/br/com/forum/controller/AutenticacaoControllerTest.java
+++ src/test/java/br/com/forum/controller/AutenticacaoControllerTest.java
@@ -2,10 +2,10 @@ package br.com.forum.controller;
 
 import java.net.URI;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -19,6 +19,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AutenticacaoControllerTest {
 
 	// @WebMvcTest não inicializa as classes de autenticação. Para injetar o mockmvc então usa-se o @AutoConfigureMockMvc
@@ -26,21 +27,41 @@ class AutenticacaoControllerTest {
 	@Autowired
 	private MockMvc mockPostman;
 	
+	/*
+	@Autowired
+	private TestEntityManager em;
+	
 	@Test
 	void devolver400CasoDadosAutenticacaoIncorretos() throws Exception {
 		URI uri = new URI("auth");
 		
-		String json = "{\"email\":\"invalido@email.com\", \"senha\":\"123456}\"";
+		Usuario usuario = new Usuario();
+		usuario.setNome("Bernardo");
+		usuario.setEmail("aluno@email.com");
+		usuario.setSenha("$2a$10$sWBvvEKgu3IocES3tU4q2OtuWTdLW8ybOtT1Ow7prWbvn5MPThDde");
+		em.persist(usuario);
+		
+		String json = "{\"email\":\"aluno@email.com\", \"senha\":\"123456}\"";
 		
 		mockPostman.perform(
 			MockMvcRequestBuilders
 				.post(uri)
 				.content(json)
 				.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(MockMvcResultMatchers.status().isBadRequest());
+		.andExpect(MockMvcResultMatchers.status().isOk());
 	}
+	*/
 	
-	@AfterEach
-	void tearDown() throws Exception {
+	@Test
+	public void devolver400CasoDadosAutenticacaoIncorretos() throws Exception {
+		URI uri = new URI("/auth");
+		String json = "{\"email\":\"invalido@email.com\",\"senha\":\"123456\"}";
+		
+		mockPostman.perform(
+				MockMvcRequestBuilders
+					.post(uri)
+					.content(json)
+					.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(MockMvcResultMatchers.status().isBadRequest());
 	}
 }
