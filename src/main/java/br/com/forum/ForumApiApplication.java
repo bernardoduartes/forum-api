2a5d924e61eacde53d317cc0c3e26386765791cc
Bernardo Duarte <bernardo.silva@infobase.com.br>
2022-07-14 14:07:12 -0300

    Revert "Deploy tradicional com arquivo war"
    This reverts commit 36b7698f90dcfd05f4d59bbbe7204077916d5924.

--- src/main/java/br/com/forum/ForumApiApplication.java
+++ src/main/java/br/com/forum/ForumApiApplication.java
@@ -2,25 +2,19 @@ package br.com.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableCaching
-@EnableSwagger2
 @SpringBootApplication
 @EnableSpringDataWebSupport
-public class ForumApiApplication  extends SpringBootServletInitializer {
+@EnableCaching
+@EnableSwagger2
+public class ForumApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApiApplication.class, args);
 	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ForumApiApplication.class);
-	}
+
 }
