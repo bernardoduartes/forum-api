36b7698f90dcfd05f4d59bbbe7204077916d5924
Bernardo Duarte <bernardo.silva@infobase.com.br>
2022-07-14 14:06:53 -0300

    Deploy tradicional com arquivo war

--- src/main/java/br/com/forum/ForumApiApplication.java
+++ src/main/java/br/com/forum/ForumApiApplication.java
@@ -2,19 +2,25 @@ package br.com.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
-@EnableSpringDataWebSupport
 @EnableCaching
 @EnableSwagger2
-public class ForumApiApplication {
+@SpringBootApplication
+@EnableSpringDataWebSupport
+public class ForumApiApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApiApplication.class, args);
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ForumApiApplication.class);
+	}
 }
